b8cacf3a4aa2f47c0cec732b611404e97df7fbee
kehailin <kehailin@hfjy.com>
2019-02-21 18:10:18 +0800

    init

--- aliyun-xue-study/src/main/scala/com/hfjy/logtail/bean/Content.scala
+++
@@ -1,9 +0,0 @@
-package com.hfjy.logtail.bean
-
-import scala.beans.BeanProperty
-
-/**
-  * Created by kehailin on 2018-11-27. 
-  */
-case class Content(@BeanProperty override val dateTime: String = "",
-                   @BeanProperty var logmessage: String = "") extends Time
